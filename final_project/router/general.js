8ae84b46c232d29416d4306760320aa325df49cf
harine-sivakumar <harinesivakumar42_bec28@mepcoeng.ac.in>
2026-06-14 09:39:17 -0400

    Completed task 3 - Get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,7 +25,17 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookKeys = Object.keys(books);
+  let authorBooks = {};
+
+  bookKeys.forEach(key => {
+    if (books[key].author === author) {
+      authorBooks[key] = books[key];
+    }
+  });
+
+  return res.send(JSON.stringify(authorBooks, null, 4));
 });
 
 // Get all books based on title
